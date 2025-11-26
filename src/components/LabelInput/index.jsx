09b48ded17a64c78d463c3e8772e6a074e906e93
Igor Josiel <igorjosielvitalino@gmail.com>
2025-11-26 15:45:00 -0300

    Ajusta os arquivos o componente de LabelInput.

--- src/components/LabelInput/index.jsx
+++ src/components/LabelInput/index.jsx
@@ -1,9 +1,9 @@
 import "./label-input.style.css";
 
-export function LabelInput ({ htmlFor, children }) {
-    return (
-        <label htmlFor={htmlFor} className="label-input">
-            {children}
-        </label>
-    );
+export default function LabelInput({ htmlFor, children, ...rest }) {
+  return (
+    <label htmlFor={htmlFor} className="label-input" {...rest}>
+      {children}
+    </label>
+  );
 }
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,74 +1,74 @@
 import { use } from "react";
-import Button from '../Button';
-import { TextInput } from '../TextInput';
+import Button from "../Button";
+import { TextInput } from "../TextInput";
 import DateInput from "../DateInput";
 import { SelectInput } from "../SelectInput";
-import TodoContext from '../TodoProvider/TodoContext';
-import { LabelInput } from "../LabelInput";
-import './todo-form.style.css';
+import TodoContext from "../TodoProvider/TodoContext";
+import LabelInput from "../LabelInput";
+import "./todo-form.style.css";
 
-const TITLE_INPUT       = "title";
+const TITLE_INPUT = "title";
 const DESCRIPTION_INPUT = "description";
-const DATE_INPUT        = "date";
-const PRIORITY_INPUT    = "priority";
+const DATE_INPUT = "date";
+const PRIORITY_INPUT = "priority";
 
-export function TodoForm ({ onSubmit }) {
-    const { selectedTodo } = use(TodoContext);
+export function TodoForm({ onSubmit }) {
+  const { selectedTodo } = use(TodoContext);
 
-    return (
-        <form className="todo-form" action={onSubmit}>
-            <LabelInput htmlFor={TITLE_INPUT}>Título</LabelInput>
-            <TextInput
-                id={TITLE_INPUT}
-                name={TITLE_INPUT}
-                placeholder="Digite um título para o item"
-                defaultValue={selectedTodo?.[TITLE_INPUT]}
-                required
-            />
+  return (
+    <form className="todo-form" action={onSubmit}>
+      <LabelInput htmlFor={TITLE_INPUT}>Título</LabelInput>
+      <TextInput
+        id={TITLE_INPUT}
+        name={TITLE_INPUT}
+        placeholder="Digite um título para o item"
+        defaultValue={selectedTodo?.[TITLE_INPUT]}
+        required
+      />
 
-            <LabelInput htmlFor={DESCRIPTION_INPUT}>Descrição</LabelInput>
-            <TextInput
-                id={DESCRIPTION_INPUT}
-                name={DESCRIPTION_INPUT}
-                placeholder="Digite uma descrição para o item"
-                defaultValue={selectedTodo?.[DESCRIPTION_INPUT]}
-                required
-            />
+      <LabelInput htmlFor={DESCRIPTION_INPUT}>Descrição</LabelInput>
+      <TextInput
+        id={DESCRIPTION_INPUT}
+        name={DESCRIPTION_INPUT}
+        placeholder="Digite uma descrição para o item"
+        defaultValue={selectedTodo?.[DESCRIPTION_INPUT]}
+        required
+      />
 
-            <LabelInput htmlFor={DATE_INPUT}>Data</LabelInput>
-            <DateInput
-                id={DATE_INPUT}
-                name={DATE_INPUT}
-                defaultValue={selectedTodo?.[DATE_INPUT]}
-                required
-            />
+      <LabelInput htmlFor={DATE_INPUT}>Data</LabelInput>
+      <DateInput
+        id={DATE_INPUT}
+        name={DATE_INPUT}
+        defaultValue={selectedTodo?.[DATE_INPUT]}
+        required
+      />
 
-            <LabelInput htmlFor={PRIORITY_INPUT}>Prioridade</LabelInput>
-            <SelectInput
-                id={PRIORITY_INPUT}
-                name={PRIORITY_INPUT}
-                defaultValue={selectedTodo?.[PRIORITY_INPUT]}
-                options={[
-                    {
-                        value: "",
-                        option: "Prioridade",
-                    },
-                    {
-                        value: "low",
-                        option: "Baixa",
-                    },
-                    {
-                        value: "medium",
-                        option: "Média",
-                    },
-                    {
-                        value: "high",
-                        option: "Alta",
-                    },
-                ]}
-            />
-            
-            <Button>Salvar Item</Button>
-        </form>
-    );
+      <LabelInput htmlFor={PRIORITY_INPUT}>Prioridade</LabelInput>
+      <SelectInput
+        id={PRIORITY_INPUT}
+        name={PRIORITY_INPUT}
+        defaultValue={selectedTodo?.[PRIORITY_INPUT]}
+        options={[
+          {
+            value: "",
+            option: "Prioridade",
+          },
+          {
+            value: "low",
+            option: "Baixa",
+          },
+          {
+            value: "medium",
+            option: "Média",
+          },
+          {
+            value: "high",
+            option: "Alta",
+          },
+        ]}
+      />
+
+      <Button>Salvar Item</Button>
+    </form>
+  );
 }
